d6f40e62c2ecd50e8057da0cf81e70f5507d670d
Emma Saarelainen <saarelainen.emma@gmail.com>
2021-07-02 14:44:59 +0300

    Muutoksia

--- Käsittelylogiikka/Käsittelylogiikka/TiedostonKäsittely.cs
+++ Käsittelylogiikka/Käsittelylogiikka/TiedostonKäsittely.cs
@@ -22,16 +22,29 @@ namespace Käsittelylogiikka
                                     "569946;AZ-301 Azure Solutions Architect Design;18.11.2019;20.11.2019;Pasila;;Opi suunnittelemaan Azure-ympäristö ja sen arkkitehtuuri.Tämä kurssi tarjoaa syventävää osaamista erityisesti sovellusarkkitehdeille.; True;Technology;Infra;Microsoft Cloud-services;" ,
                                     "569947;AZ-301 Azure Solutions Architect Design;18.11.2019;20.11.2019;Online;;Opi suunnittelemaan Azure-ympäristö ja sen arkkitehtuuri.Tämä kurssi tarjoaa syventävää osaamista erityisesti sovellusarkkitehdeille.; True;Technology;Infra;Microsoft Cloud-services;" };
 
-        public void Alkupaiva()
+        public bool Alkupaiva(DateTime alkupäivä, DateTime loppupäivä)
         {
-
+            DateTime tänään = new DateTime.Today;
+            var tänään2019 = tänään.AddYears(-2);
+            if(alkupäivä - tänään2019 < 0)
+            {
+                return false;
+            }
+            if(loppupäivä - alkupäivä < 0)
+            {
+                return false;
+            }
+            // viedään lokiin
+            return true;
         }
-        public void MaksimiPituus(string kuvaus)
+        public bool MaksimiPituus(string kuvaus)
         {
-            if(kuvaus > 500)
+            if(kuvaus.Length > 500)
             {
                 //viedään lokiin
+                return false
             }
+            return true
         }
 
     }
